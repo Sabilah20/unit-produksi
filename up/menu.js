01d9c3929df40be3c9bf61d32e153a0f3f312026
Sabilah20 <113427996+Sabilah20@users.noreply.github.com>
2022-11-03 08:37:09 +0700

    Add files via upload

---
+++ up/menu.js
@@ -0,0 +1,14 @@
+// const btnTmbh = document.getElementById('tmbh')
+// const showTmbh = document.getElementById('show')
+
+// btnTmbh.setAttribute('click', () => {
+//     showTmbh.style.display = 'block'
+//     btnTmbh.style.display = 'none'
+// })
+
+function show() {
+    const btnTmbh = document.getElementById('tmbh')
+    const showTmbh = document.getElementById('show')
+    showTmbh.style.display = 'block'
+    btnTmbh.style.display = 'none'
+}
